0d5b69403b9c85fdff3749014d7fea6413cce0d4
Nesara Shree <54513341+Nesara13@users.noreply.github.com>
2023-03-22 21:22:35 -0700

    fmnist and mnist examples

---
+++ fmnist.py
@@ -0,0 +1,154 @@
+# imports
+import snntorch as snn
+from snntorch import surrogate
+from snntorch import backprop
+from snntorch import functional as SF
+from snntorch import utils
+from snntorch import spikeplot as splt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.utils.data import Subset
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+# dataloader arguments
+batch_size = 128
+data_path='data/fmnist'
+
+dtype = torch.float
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# Define a transform
+transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+indices10k = torch.arange(1000)
+indices5k = torch.arange(500)
+mnist_train_10k = Subset(mnist_train, indices10k)
+mnist_test_5k = Subset(mnist_train, indices5k)
+
+print(f"mnist_train is: {len(mnist_train):.2f}%")
+print(f"mnist_test is: {len(mnist_test):.2f}%")
+print(f"mnist_train is: {len(mnist_train_10k):.2f}%")
+print(f"mnist_test is: {len(mnist_test_5k):.2f}%")
+
+
+# Create DataLoaders
+#train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+#test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
+train_loader = DataLoader(mnist_train_10k, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test_5k, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
+# neuron and simulation parameters
+spike_grad = surrogate.fast_sigmoid(slope=25)
+beta = 0.5
+num_steps = 50
+
+#  Initialize Network
+net = nn.Sequential(nn.Conv2d(1, 12, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Conv2d(12, 64, 5),
+                    nn.MaxPool2d(2),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+                    nn.Flatten(),
+                    nn.Linear(64*4*4, 10),
+                    snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True, output=True)
+                    ).to(device)
+
+# net = nn.Sequential(
+#     nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+#     snn.Leaky(beta=beta, spike_grad=spike_grad, init_hidden=True),
+#     nn.ReLU(),
+#     nn.MaxPool2d(kernel_size=2),
+#     nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+#     nn.ReLU(),
+#     nn.MaxPool2d(kernel_size=2),
+#     nn.Flatten(),
+#     nn.Linear(in_features=32 * 8 * 8, out_features=10)
+# ).to(device)
+
+
+data, targets = next(iter(train_loader))
+data = data.to(device)
+targets = targets.to(device)
+
+for step in range(num_steps):
+    spk_out, mem_out = net(data)
+
+def forward_pass(net, num_steps, data):
+  mem_rec = []
+  spk_rec = []
+  utils.reset(net)  # resets hidden states for all LIF neurons in net
+
+  for step in range(num_steps):
+      spk_out, mem_out = net(data)
+      spk_rec.append(spk_out)
+      mem_rec.append(mem_out)
+  
+  return torch.stack(spk_rec), torch.stack(mem_rec)
+
+spk_rec, mem_rec = forward_pass(net, num_steps, data)
+
+# already imported snntorch.functional as SF 
+loss_fn = SF.ce_rate_loss()
+
+loss_val = loss_fn(spk_rec, targets)
+
+print(f"The loss from an untrained network is {loss_val.item():.3f}")
+
+acc = SF.accuracy_rate(spk_rec, targets)
+
+print(f"The accuracy of a single batch using an untrained network is {acc*100:.3f}%")
+
+def batch_accuracy(train_loader, net, num_steps):
+  with torch.no_grad():
+    total = 0
+    acc = 0
+    net.eval()
+    
+    train_loader = iter(train_loader)
+    for data, targets in train_loader:
+      data = data.to(device)
+      targets = targets.to(device)
+      spk_rec, _ = forward_pass(net, num_steps, data)
+
+      acc += SF.accuracy_rate(spk_rec, targets) * spk_rec.size(1)
+      total += spk_rec.size(1)
+
+  return acc/total
+
+test_acc = batch_accuracy(test_loader, net, num_steps)
+
+print(f"The total accuracy on the test set is: {test_acc * 100:.2f}%")
+
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-2, betas=(0.9, 0.999))
+num_epochs = 10
+test_acc_hist = []
+
+# training loop
+for epoch in range(num_epochs):
+
+    avg_loss = backprop.BPTT(net, train_loader, optimizer=optimizer, criterion=loss_fn, 
+                            num_steps=num_steps, time_var=False, device=device)
+    
+    print(f"Epoch {epoch}, Train Loss: {avg_loss.item():.2f}")
+
+    # Test set accuracy
+    test_acc = batch_accuracy(test_loader, net, num_steps)
+    test_acc_hist.append(test_acc)
+
+    print(f"Epoch {epoch}, Test Acc: {test_acc * 100:.2f}%\n")
+
